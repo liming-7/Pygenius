8989b9372510e396336a030349e6106578602f8f
ming <uestcliming@gmail.com>
2020-04-10 21:49:50 +0200

    Ming

--- core/issuevalue.py
+++ core/issuevalue.py
@@ -4,6 +4,7 @@
 # @FileName: issuevalue.py
 # @Software: PyCharm
 
+import re
 from typing import Any, Callable, Dict, List, Optional, Set, Union
 
 class Domain:
@@ -11,6 +12,10 @@ class Domain:
     def __init__(self, domainName: str, issueValueSet: dict):
         if domainName == None:
             raise ValueError("Domain name is none!")
+
+        if not re.match('[a-zA-Z0-9]+', domainName):
+            raise ValueError(
+                'domain name can have only simple characters but found ' + domainName)
 
         if issueValueSet == None:
             raise ValueError('issues is none!')
@@ -22,14 +27,57 @@ class Domain:
         self.name = domainName
         self.issueValueSet = issueValueSet
 
+    def toString(self):
+        return 'Domain[' + self.name + ',' + self.issueValueSet + ']'
+
     def getName(self):
         return self.name
 
     def getIssues(self):
         return list(self.issueValueSet)
 
+    def getValueSet(self, issue: str): #可以加一个判断
+        return self.issueValueSet[issue]
 
-class Issue:
+    def isComplete(self, bid):
+        if not isinstance(bid, Bid):
+            raise TypeError('input should be a Bid!')
+
+        if list(self.issueValueSet) != bid.getIssues():
+            return 'Issues in bid (' + bid.getIssues() \
+                   + ') do not match issues in domain (' + list(self.issueValueSet) + ')'
+
+        for issue in list(self.issueValueSet):
+            if bid.getValue(issue) not in self.issueValueSet[issue]:
+                return 'bid issue ' + issue + 'has illegal value' + bid.getValue(issue)
+
+        return True
+
+    def hashCode(self):
+        pass
+
+    def equals(self):
+        pass
+
+class Issue: #seems not necessary
+    pass
+
+class Value:
+    pass
+
+class ValueSet:
+    pass
+
+class DiscreteValue(Value):
+    pass
+
+class DiscreteValueSet(ValueSet):
+    pass
+
+class NumberValue(Value):
+    pass
+
+class NumeberValueSet(ValueSet):
     pass
 
 class Bid:
@@ -52,6 +100,11 @@ class Bid:
         return len(self.issuevalue)
 
     def contianIssue(self, issue: str):
+        '''
+        description
+        :param issue:
+        :return:
+        '''
         return issue in self.issuevalue
 
     def getValue(self, issue: str):
--- core/party.py
+++ core/party.py
@@ -3,3 +3,47 @@
 # @Author  : Ming Li
 # @FileName: party.py
 # @Software: PyCharm
+
+import re
+from typing import Any, Callable, Dict, List, Optional, Set, Union
+
+protocol_Set = Set[str]
+
+class Capabilities:
+
+    def __init__(self, protocol: protocol_Set):
+        '''
+
+        :param protocol: Set of supported protocols.
+        '''
+        if protocol == None:
+            raise ValueError('suppoeted protocol is None!')
+        self.supportProtocol = protocol
+
+    def getSupportProtocol(self) -> protocol_Set:
+        return self.supportProtocol
+
+    def hasCode(self):
+        pass
+
+    def equals(self):
+        pass
+
+class Party:
+    '''
+    the basic party
+    '''
+    def __init__(self):
+        pass
+
+class SimulateParty(Party):
+    '''
+    use in algorithm simulation
+    '''
+    pass
+
+class WebParty(Party):
+    '''
+    use in real negotiation nodes via Web service
+    '''
+    pass
