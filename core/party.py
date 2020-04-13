b5df68d3b329c71346173bac79e8cc36ca72b4ea
ming <uestcliming@gmail.com>
2020-04-14 00:32:22 +0200

    4.14

--- core/party.py
+++ core/party.py
@@ -4,6 +4,9 @@
 # @FileName: party.py
 # @Software: PyCharm
 
+#要考虑到不同名字的agent可能会用一种strategy， 并且在进行过程中方便随时更换新的strategy，
+# 同时需要考虑strategy可以在negotiation中可以更新， 因而对strategy与agent进行分离。
+
 import re
 from typing import Any, Callable, Dict, List, Optional, Set, Union
 
@@ -13,7 +16,15 @@ protocol_Set = Set[str]
 
 
 class Strategy:
-    pass
+    # All strategy should have this
+    def __init__(self):
+        self.checkComplement()
+
+    def chooseAction(self):
+        pass
+
+    def checkComplement(self): #查找是否有chooseAction这个函数，或者一些其他函数。
+        pass
 
 
 class UtilitySpace:
@@ -25,7 +36,7 @@ class Party:
     the basic party
     '''
 
-    def __init__(self, name, capabilities: protocol_Set):
+    def __init__(self, name, capabilities: protocol_Set, strategy = None, utilityspace = None):
         if not isinstance(name, str):
             raise TypeError('input name should be string but got ' + str(type(name)))
         if not re.match('[a-zA-Z]\\w*', name):
@@ -34,9 +45,10 @@ class Party:
         if capabilities is None:
             raise ValueError('suppoeted protocol is None!')
 
-
         self.name = name
         self.supprotocol = capabilities
+        self.strategy = strategy
+        self.utilitySpace = utilityspace
 
     def setStrategy(self, strategy):
         self.strategy = strategy
@@ -73,4 +85,8 @@ class WebParty(Party):
     def __init__(self, name, capabilities):
         super().__init__(name, capabilities)
 
-    def seril
+    def serialize(self):
+        pass
+
+    def deserialize(self):
+        pass
---
+++ core/protocol.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 14/04/2020 00:27
+# @Author  : Ming Li
+# @FileName: protocol.py
+# @Software: PyCharm
+
+# this is a protocol library.
